a03dd0e093862cfec4d0e091fd9a9cbe6ab30100
Rogério Augusto <rogerio@depoiseuleio.com>
2010-09-06 18:45:08 -0300

    checkpoint

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -38,8 +38,24 @@ class Admin::ReportsController < Admin::ApplicationController
   end
   
   def snapshot_by_assignment
-    @users = User.all.reject{|u| u.assigned_opportunities.count < 1}
-    @series = @users.collect(&:opportunity_report)
+    # @users = User.all.reject{|u| u.assigned_opportunities.count < 1}
+    # @series = @users.collect(&:opportunity_report)
+    
+    @bar = HighChart.new('column') do |f|
+          f.series(:name=>'John',:data=> [3, 20, 3, 5, 4, 10, 12 ])
+          f.series(:name=>'Jane',:data=>[1, 3, 4, 3, 3, 5, 4,-46] )		
+          f.title({ :text=>"example test title from controller"})
+
+          ###  Options for Bar
+          ### f.options[:chart][:defaultSeriesType] = "bar"
+          ### f.plot_options({:series=>{:stacking=>"normal"}}) 
+
+          ## or options for column
+          f.options[:chart][:defaultSeriesType] = "column"
+          f.plot_options({:column=>{:stacking=>"percent"}})
+        end
+    
+    
   end
 
 end
